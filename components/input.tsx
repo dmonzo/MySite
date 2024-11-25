1ecfe42858c035c422847a6b3d39e0f23719141e
Dashiel Monzo <dashmonzo101@gmail.com>
2024-11-25 01:25:44 -0500

    for vercel deployment

--- components/input.tsx
+++ components/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  // You can add custom props here if needed in the future
+  // Add custom props here if needed in the future
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
--- components/textarea.tsx
+++ components/textarea.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  // You can add custom props here if needed in the future
+  // Add custom props here if needed in the future
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
